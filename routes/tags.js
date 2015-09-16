759aeab9f7877fcd1934fb7e86b48c687b202aeb
konstantin <krivlenia@targetprocess.com>
2015-09-16 19:59:07 +0300

    unique tags

--- routes/tags.js
+++ routes/tags.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fs = require("fs");
+var _ = require("lodash");
 /* GET users listing. */
 router.post('/', function (req, res) {
     res.format({
@@ -10,7 +11,7 @@ router.post('/', function (req, res) {
                 if (!err) {
                     tags = JSON.parse(buffer.toString());
                 }
-                fs.writeFile('tags.json', JSON.stringify(tags.concat(data.body)), function () {
+                fs.writeFile('tags.json', JSON.stringify(_.unique(tags.concat(data.body))), function () {
                     res.send({message: 'write'});
                 });
             });
